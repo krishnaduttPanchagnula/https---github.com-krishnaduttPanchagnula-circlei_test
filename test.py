adaea90112742be9c5fc32decae12ff55213016b
krishnaduttPanchagnula <krishnadutt123@gmail.com>
2022-01-23 18:14:16 +0530

    added test

--- test.py
+++ test.py
@@ -2,7 +2,17 @@ import calc
 
 def test():
     assert calc.add(10, 5) == 15
-    print ( 'ran perfectly')
+    print ( 'Add ran perfectly')
+
+    assert calc.divide(10, -1) == -10
+    print ("divide ran sucessfully")
+
+    assert calc.subtract(10, -1) == 11
+    print ("subtract ran sucessfully")
+
+    assert calc.multiply(5, -1) == -5
+    print ("multiply ran sucessfully")
+
 
 if __name__ == '__main__':
     test()
